4bb15a00793b4055ff0c34f63e776f7f2b91a185
roger <roger.herikstad@gmail.com>
2020-09-15 17:06:20 +0800

    Updates code

--- src/RemoteFiles.jl
+++ src/RemoteFiles.jl
@@ -1,36 +1,30 @@
 module RemoteFiles
 
 const lib = "/usr/local/lib/libssh.dylib"
+const lib2 = "src/remote_files.dylib"
 
 mutable struct SSHSession
 end
 
-@enum SSHOption  SSH_OPTIONS_HOST=0 SSH_OPTIONS_PORT=1 SSH_OPTIONS_PORT_STR=2 SSH_OPTIONS_FD=3 SSH_OPTIONS_USER=4
-
-function ssh_session() 
-    output_ptr = ccall((:ssh_new, lib), Ptr{SSHSession}, ())
-    if output_ptr == C_NULL 
-        throw(error("Could no create session"))
-      end
-    return output_ptr
+function ssh_session(hostname::String, port::Int64=22) 
+    session = ccall((:connect_to_host, lib2), Ptr{SSHSession}, (Cstring, Cint), hostname, port)
+    if session == C_NULL
+        error("Could not connect to host")
+    end
+    return session
 end
 
-function Base.open(session::Ref{SSHSession}, hostname::String, username::String, port::Int64=22)
-    ccall((:ssh_options_set, lib), Cint, (Ref{SSHSession},Cint, Cstring), session, SSH_OPTIONS_HOST, hostname)    
-    ccall((:ssh_options_set, lib), Cint, (Ref{SSHSession},Cint, Cstring), session, SSH_OPTIONS_USER, username)    
-    ccall((:ssh_options_set, lib), Cint, (Ref{SSHSession},Cint, Ref{Cint}), session, SSH_OPTIONS_PORT, port)    
+function ssh_session(func::Function, hostname::String, port::Int64)
+    session = ssh_session(hostname, port)
+    try
+        func(session)
+    finally
+        disconnect(session)
+    end
 end
 
-function connect(session::Ref{SSHSession})
-    ccall((:ssh_connect, lib), Cint, (Ref{SSHSession},), session)
-end
-
-function disconnect(session::Ref{SSHSession})
-    ccall((:ssh_disconnect, lib), Cint, (Ref{SSHSession},), session)
-end
-
-function free(session::Ref{SSHSession})
-    ccall((:ssh_free, lib), Cvoid, (Ref{SSHSession},), session)
+function disconnect(session::Ptr{SSHSession})
+    ccall((:disconnect, lib2), Cint, (Ptr{SSHSession},), session)
 end
 
 end # module
