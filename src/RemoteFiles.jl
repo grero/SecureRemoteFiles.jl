944db47df732998216de2e5f278b05fa32a3577c
roger <roger.herikstad@gmail.com>
2020-09-17 12:34:03 +0800

    Changes to get basic reading of JLD2 files over SFTP to work

--- src/RemoteFiles.jl
+++ src/RemoteFiles.jl
@@ -1,4 +1,5 @@
 module RemoteFiles
+using JLD2
 
 const lib = "/usr/local/lib/libssh.dylib"
 const lib2 = "deps/remote_files.dylib"
@@ -9,7 +10,25 @@ end
 mutable struct SFTPSession
 end
 
+mutable struct SFTPFileHandle
+end
+
+mutable struct SFTPAttributes
+end
+
 mutable struct SFTPFile <: IO
+    handle::Ptr{SFTPFileHandle}
+    _isopen::Bool
+end
+
+include("dataio.jl")
+include("buffered.jl")
+include("jld2.jl")
+
+function ssh_version()
+    _version = ccall((:ssh_version, lib), Ptr{UInt8}, (Cint,), 0)
+    vstring = unsafe_string(_version)
+    return vstring
 end
 
 function ssh_session(hostname::String, port::Int64=22)
@@ -60,15 +79,15 @@ function sftp_session(func::Function, ssh_session)
 end
 
 function sftp_open(session::Ptr{SFTPSession}, fname::String, access_type::Int64)
-    file = ccall((:sftp_open, lib2),Ptr{SFTPFile}, (Ptr{SFTPSession}, Cstring, Cint), session, fname, access_type)
+    file = ccall((:sftp_open, lib2),Ptr{SFTPFileHandle}, (Ptr{SFTPSession}, Cstring, Cint), session, fname, access_type)
     if file == C_NULL
         error("Could not open remote file $fname")
     end
     return file
 end
 
-function sftp_close(file::Ptr{SFTPFile})
-    rc = ccall((:sftp_close, lib2), Cint, (Ptr{SFTPFile},), file)
+function sftp_close(file::Ptr{SFTPFileHandle})
+    rc = ccall((:sftp_close, lib2), Cint, (Ptr{SFTPFileHandle},), file)
     if rc != 0
         error("Could not close remote file")
     end
@@ -83,28 +102,111 @@ function sftp_open(func::Function, sftp_session::Ptr{SFTPSession}, fname, access
     end
 end
 
-function sftp_read(file::Ptr{SFTPFile},nbytes::Int64)
+function sftp_read(file::Ptr{SFTPFileHandle},nbytes::Int64)
     buffer = fill(zero(UInt8), nbytes)
     sftp_read(file, buffer)
 end
 
-function sftp_read(file::Ptr{SFTPFile}, buffer::Vector{UInt8})
+function sftp_read(file::Ptr{SFTPFileHandle}, buffer::Vector{UInt8})
     nbytes = length(buffer)
-    bytes_read = ccall((:sftp_read, lib), Cint, (Ptr{SFTPFile}, Ref{UInt8}, Cint), file, buffer, nbytes)
+    bytes_read = ccall((:sftp_read, lib), Cint, (Ptr{SFTPFileHandle}, Ref{UInt8}, Cint), file, buffer, nbytes)
     if bytes_read == 0
         return UInt8[]
     end
     return buffer[1:bytes_read]
 end
 
-function sftp_seek(file::Ptr{SFTPFile}, pos::Int64)
-    rc = ccall((:sftp_seek, lib), Cint, (Ptr{SFTPFile}, Cint), file, pos)
+function sftp_seek(file::Ptr{SFTPFileHandle}, pos::UInt64)
+    rc = ccall((:sftp_seek, lib), Cint, (Ptr{SFTPFileHandle}, Culonglong), file, pos)
     if rc != 0
         error("Failed to seek to desired position")
     end
 end
 
-Base.seek(file::Ptr{SFTPFile}, pos) = sftp_seek(file, pos)
-Base.read!(file::Ptr{SFTPFile}, data::Vector{UInt8}) = sftp_read(file, data)
+function sftp_tell(file::Ptr{SFTPFileHandle})
+    pos = ccall((:sftp_tell64, lib), Culonglong, (Ptr{SFTPFileHandle},), file)
+    return pos
+end
+
+Base.seek(file::SFTPFile, pos) = sftp_seek(file.handle, UInt64(pos))
+Base.read!(file::SFTPFile, data::Vector{UInt8}) = sftp_read(file.handle, data)
+Base.read(file::SFTPFile, ::Type{UInt8}) = (c = sftp_read(file.handle, 1); length(c) == 1 ? first(c) : nothing)
+Base.close(file::SFTPFile) = (file._isopen && sftp_close(file.handle);file._isopen = false)
+Base.isopen(file::SFTPFile) = file._isopen
+
+# this is a hack
+function Base.eof(file::SFTPFile)
+    buffer = Vector{UInt8}(undef, 1) 
+    n = sftp_read(file.handle, buffer)
+    if n == 0
+        return true
+    else
+        pos = sftp_tell(file.handle)
+        sftp_seek(file.handle, pos-1)
+    end
+end
+
+function readuntil(s::SFTPFile, delim::T; keep::Bool=false) where T
+    out = (T === UInt8 ? Base.StringVector(0) : Vector{T}())
+    while true
+        c = read(s, T)
+        if c == delim
+            keep && push!(out, c)
+            break
+        elseif c == nothing
+            break
+        end
+        push!(out, c)
+    end
+    return out
+end
+
+function sftp_fstat(file::Ptr{SFTPFileHandle})
+    _fstat = ccall((:sftp_fstat, lib), Ptr{SFTPAttributes}, (Ptr{SFTPFileHandle},), file)
+    if _fstat == C_NULL
+        error("Could not get fstats for file")
+    end
+    return _fstat
+end
+
+function Base.stat(io::SFTPFile)
+    fstat = sftp_fstat(io.handle)
+    bufptr = convert(Ptr{UInt8}, _fstat)
+    bytes = Vector{UInt8}(undef, 8)
+    for i in 1:length(bytes) 
+        bytes[i] = unsafe_load(bufptr, 24+i)
+    end
+    fsize = first(reinterpret(UInt64, bytes))
+end
+
+function sftp_filesize(file::Ptr{SFTPFileHandle})
+    _fstat = sftp_fstat(file)
+    bufptr = convert(Ptr{UInt8}, _fstat)
+    bytes = Vector{UInt8}(undef, 8)
+    for i in 1:length(bytes) 
+        bytes[i] = unsafe_load(bufptr, 24+i)
+    end
+    fsize = reinterpret(UInt64, bytes)
+    first(fsize)
+end
+
+function sftp_filename(file::Ptr{SFTPFileHandle})
+    _fstat = sftp_fstat(file)
+    bufptr = unsafe_load(convert(Ptr{UInt64}, _fstat),2)
+    @show bufptr
+    name = unsafe_string(convert(Ptr{UInt8}, bufptr[1]))
+    return name
+end
+
+function Base.filesize(io::SFTPFile)
+    sftp_filesize(io.handle)
+end
+
+function Base.position(io::SFTPFile)
+    sftp_tell(io.handle)
+end
+
+# reads are always blocking for now
+Base.bytesavailable(io::SFTPFile) = 0
 
 end # module
--- src/jld2.jl
+++ src/jld2.jl
@@ -5,26 +5,38 @@ JLD2.read_bytestring(io::SFTPFile) = String(readuntil(io, 0x0))
 
 function JLD2.openfile(::Type{SFTPFile}, session::Ptr{SFTPSession}, fname, wr, create, truncate, fallback::Nothing=nothing)
     access = 0
-    sftp_open(session, fname, access)    
+    handle = sftp_open(session, fname, access)
+    SFTPFile(handle, true)
 end
 
-function JLD2.jldopen(fname::AbstractString, wr::Bool, create::Bool, truncate::Bool, ::Type{SFTPFile}, fallback=nothing, compress::Bool=false, mmaparrays::Bool=false, session::Ptr{SFPSession})
+function JLD2.jldopen(fname::AbstractString, wr::Bool, create::Bool, truncate::Bool, ::Type{SFTPFile}, session::Ptr{SFTPSession},fallback=nothing, compress::Bool=false, mmaparrays::Bool=false)
     # exist fname
-    created = true
-    io = openfile(SFTPFile, session, fname, wr, create, truncate, fallback)
+    created = false 
+    io = JLD2.openfile(SFTPFile, session, fname, wr, create, truncate, fallback)
     f = JLDFile(io, fname, wr, created, compress, mmaparrays)
     JLD2.OPEN_FILES[fname] = WeakRef(f)
+    f.n_times_opened = 1
     if created
         f.root_group = JLD2.Group{typeof(f)}(f)
         f.types_group = JLD2.Group{typeof(f)}(f)
     else
         verify_file_header(f)
         seek(io, JLD2.FILE_HEADER_LENGTH)
-        superblock = read(io, Superblock)
+        superblock = read(io, JLD2.Superblock)
     f.end_of_data = superblock.end_of_file_address
     f.root_group_offset = superblock.root_group_object_header_address
     f.root_group = load_group(f, superblock.root_group_object_header_address)
+    f
     end
+end
 
+
+function JLD2.jld_finalizer(f::JLD2.JLDFile{SFTPFile})
+    f.n_times_opened == 0 && return
+    if f.written && !isopen(f.io)
+        f.io = openfile(SFTPFile, f.path, true, false, false)
+    end
+    f.n_times_opened = 1
+    close(f)
 end
 
