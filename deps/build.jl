bdefe873c216701096126e836257347cc15cce88
roger <roger.herikstad@gmail.com>
2020-09-25 15:53:14 +0800

    Tweaks build script

--- deps/build.jl
+++ deps/build.jl
@@ -1,1 +1,3 @@
-run(`gcc -fPIC --shared ../src/remote_files.c -o remote_files.dylib -lssh`)
+library_path = get(ENV, "LD_LIBRARY_PATH", "")
+include_path = get(ENV, "C_INCLUDE_PATH", "")
+run(`gcc -fPIC --shared -I$(include_path) -L$(library_path) ../src/remote_files.c -o remote_files.dylib -lssh`)
